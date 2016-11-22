51225a7e97d7847bf1a2b96bbda9ffb18b220763
Mike Vieira <mikevieira@hotmail.com>
2016-11-22 20:14:01 +0000

    devise gem added

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'kaminari'
 
 gem 'simple_form'
 
+gem 'devise', '>= 3.2.4'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+
+  config.action_mailer.default_url_options = { host: ' mikester-mike-vieira.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
