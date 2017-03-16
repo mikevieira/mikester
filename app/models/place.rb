e4a17ae6b09382dc8e61434e049d0c76ee1be806
Mike Vieira <mikevieira@hotmail.com>
2017-03-16 19:20:37 +0000

    dependant destroy and edits

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ActiveRecord::Base
-  has_many :photos
+  has_many :photos, dependent: :destroy
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   geocoded_by :address 
   after_validation :geocode
   validates :name, presence: true, length: {minimum: 4} 
